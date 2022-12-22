6082e8c7b0ba00aa7e6d5327dbb1756f49cafd9b
viratsathish1999 <sathish.r9688@gmail.com>
2022-12-22 10:25:21 +0530

    add file

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+
+   var a={}
+   a.name=prompt("enter name")
+   a.age=parseInt(prompt("enter age"))
+   a.place=prompt("enter place")
+   a.degree=prompt("enter degree")
+   
+   document.write(a)
+
+   
